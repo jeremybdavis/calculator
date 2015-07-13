a44c2fcd0da10fca9bf8a0e85549e2a9c3c3fcce
jeremybdavis <jdavis129@gmail.com>
2015-07-12 23:39:10 -0500

    Added new font and some styling

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 var $calculateBtn = document.getElementById('calculateBtn');
 var $answer = document.getElementById('answer');
 
-
 var calculate = function() {
   var $numOne = document.getElementById('num_one').value;
   var $numTwo = document.getElementById('num_two').value;
 
   var total = $numOne * $numTwo;
   $answer.innerHTML = total;
-
   return total;
-
 };
 
 $calculateBtn.onclick = calculate;
